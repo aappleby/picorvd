f73e8f6673854c4e056a1982cb9f64332e78b3fd
Austin Appleby <aappleby@gmail.com>
2023-03-05 21:13:01 -0800

    checkpoint

--- main.cpp
+++ main.cpp
@@ -95,27 +95,6 @@ int main() {
 
   int rep = 0;
 
-  const uint8_t DEG_Abst_DATA0   = 0x04;
-  const uint8_t DEG_Abst_DATA1   = 0x05;
-  const uint8_t DEG_Module_CTRL  = 0x10;
-  const uint8_t DEG_Module_STA   = 0x11;
-  const uint8_t DEG_Hart_INFO    = 0x12;
-  const uint8_t DEG_Abst_STA     = 0x16;
-  const uint8_t DEG_Abst_CMD     = 0x17;
-  const uint8_t DEG_Abst_CMDAUTO = 0x18;
-  const uint8_t DEG_Prog_BUF0    = 0x20;
-  const uint8_t DEG_Prog_BUF1    = 0x21;
-  const uint8_t DEG_Prog_BUF2    = 0x22;
-  const uint8_t DEG_Prog_BUF3    = 0x23;
-  const uint8_t DEG_Prog_BUF4    = 0x24;
-  const uint8_t DEG_Prog_BUF5    = 0x25;
-  const uint8_t DEG_Prog_BUF6    = 0x26;
-  const uint8_t DEG_Prog_BUF7    = 0x27;
-  const uint8_t DEG_Hart_SUM0    = 0x40;
-  const uint8_t ONLINE_CAPR_STA  = 0x7C;
-  const uint8_t ONLINE_CFGR_MASK = 0x7D;
-  const uint8_t ONLINE_CFGR_SHAD = 0x7E;
-
   // wch-link sequence COLD BOOT
   // 2000 us low
   //    6 ns high
@@ -191,6 +170,27 @@ int main() {
 
   pio_sm_set_enabled(pio0, sm, true);
 
+  const uint8_t DEG_Abst_DATA0   = 0x04;
+  const uint8_t DEG_Abst_DATA1   = 0x05;
+  const uint8_t DEG_Module_CTRL  = 0x10;
+  const uint8_t DEG_Module_STA   = 0x11;
+  const uint8_t DEG_Hart_INFO    = 0x12;
+  const uint8_t DEG_Abst_STA     = 0x16;
+  const uint8_t DEG_Abst_CMD     = 0x17;
+  const uint8_t DEG_Abst_CMDAUTO = 0x18;
+  const uint8_t DEG_Prog_BUF0    = 0x20;
+  const uint8_t DEG_Prog_BUF1    = 0x21;
+  const uint8_t DEG_Prog_BUF2    = 0x22;
+  const uint8_t DEG_Prog_BUF3    = 0x23;
+  const uint8_t DEG_Prog_BUF4    = 0x24;
+  const uint8_t DEG_Prog_BUF5    = 0x25;
+  const uint8_t DEG_Prog_BUF6    = 0x26;
+  const uint8_t DEG_Prog_BUF7    = 0x27;
+  const uint8_t DEG_Hart_SUM0    = 0x40;
+  const uint8_t ONLINE_CAPR_STA  = 0x7C;
+  const uint8_t ONLINE_CFGR_MASK = 0x7D;
+  const uint8_t ONLINE_CFGR_SHAD = 0x7E;
+
   while(1) {
     printf("\n");
     printf("rep   %04d\n", rep++);
@@ -199,23 +199,21 @@ int main() {
     iobank0_hw->io[LED_PIN].ctrl = GPIO_FUNC_SIO << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB;
     sio->gpio_clr    = LED_BIT;
     sio->gpio_oe_set = LED_BIT;
-    sleep_ms(5);
+    sleep_ms(2);
     sio->gpio_oe_clr = LED_BIT;
     iobank0_hw->io[LED_PIN].ctrl = GPIO_FUNC_PIO0 << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB;
 
-                  sl_put(0x7E, 0x5AA50400);    sleep_us(5);
-                  sl_put(0x7D, 0x5AA50400);    sleep_us(3);
-    uint32_t r1 = sl_get(0x7C);                sleep_us(8);
-    uint32_t r2 = sl_get(0x11);                sleep_us(10);
-                  sl_put(0x10, 0x80000001);    sleep_us(6);
-                  sl_put(0x10, 0x80000001);    sleep_us(4);
-    uint32_t r3 = sl_get(0x16);
+                  sl_put(ONLINE_CFGR_SHAD, 0x5AA50400);
+                  sl_put(ONLINE_CFGR_MASK, 0x5AA50400);
+    uint32_t r1 = sl_get(ONLINE_CAPR_STA);
     
-    sleep_us(5000);
-
-    uint32_t r4 = sl_get(0x11);                sleep_us(9);
-    uint32_t r5 = sl_get(0x7F);                sleep_us(11);
-                  sl_put(0x05, 0x1FFFF7C4);    sleep_us(6);
+    uint32_t r2 = sl_get(DEG_Module_STA);
+    sl_put(DEG_Module_CTRL, 0x80000001);
+    sl_put(DEG_Module_CTRL, 0x80000001);
+    uint32_t r3 = sl_get(DEG_Abst_STA);
+    
+    uint32_t r4 = sl_get(DEG_Module_STA);
+    uint32_t r5 = sl_get(0x7F);
 
 
     // load the 32-bit memory read program
@@ -228,289 +226,62 @@ int main() {
     sl_put(0x26, 0x25F37B20);
     sl_put(0x27, 0x90027B30);
 
-                  sl_put(0x17, 0x00040000);    sleep_us(4);
-    uint32_t r6 = sl_get(0x04);                sleep_us(12);
-                  sl_put(0x04, 0x00000003);    sleep_us(6);
-                  sl_put(0x17, 0x002307C0);    sleep_us(500);
+    // read part type
 
+    sl_put(DEG_Abst_DATA1, 0x1FFFF7C4);
+    sl_put(DEG_Abst_CMD,   0x00040000);
+    uint32_t r6 = sl_get(DEG_Abst_DATA0);
+
+    //----------
+    // stop watchdogs
+
+    sl_put(DEG_Abst_DATA0, 0x00000003);
+    sl_put(DEG_Abst_CMD,   0x002307C0);
+
+    //----------
     // part reg read
 
-    sl_put(0x05, 0x1FFFF7E0); sleep_us(10);
-    sl_put(0x17, 0x00040000); sleep_us(10);
-    uint32_t r7 = sl_get(0x04);
+    sl_put(DEG_Abst_DATA1, 0x1FFFF7E0);
+    sl_put(DEG_Abst_CMD,   0x00040000);
+    uint32_t r7 = sl_get(DEG_Abst_DATA0);
 
-    sl_put(0x05, 0x1FFFF7E8); sleep_us(10);
-    sl_put(0x17, 0x00040000); sleep_us(10);
-    uint32_t r8 = sl_get(0x04);
+    sl_put(DEG_Abst_DATA1, 0x1FFFF7E8);
+    sl_put(DEG_Abst_CMD,   0x00040000);
+    uint32_t r8 = sl_get(DEG_Abst_DATA0);
 
-    sl_put(0x05, 0x1FFFF7EC); sleep_us(10);
-    sl_put(0x17, 0x00040000); sleep_us(10);
-    uint32_t r9 = sl_get(0x04);
+    sl_put(DEG_Abst_DATA1, 0x1FFFF7EC);
+    sl_put(DEG_Abst_CMD,   0x00040000);
+    uint32_t r9 = sl_get(DEG_Abst_DATA0);
 
-    sl_put(0x05, 0x1FFFF7F0); sleep_us(10);
-    sl_put(0x17, 0x00040000); sleep_us(10);
-    uint32_t r10 = sl_get(0x04);
+    sl_put(DEG_Abst_DATA1, 0x1FFFF7F0);
+    sl_put(DEG_Abst_CMD,   0x00040000);
+    uint32_t r10 = sl_get(DEG_Abst_DATA0);
 
-    sl_put(0x05, 0x1FFFF7C4); sleep_us(10);
-    sl_put(0x17, 0x00040000); sleep_us(10);
-    uint32_t r11 = sl_get(0x04);
+    sl_put(DEG_Abst_DATA1, 0x1FFFF7C4);
+    sl_put(DEG_Abst_CMD,   0x00040000);
+    uint32_t r11 = sl_get(DEG_Abst_DATA0);
 
-    printf("r1 0x00010403 = 0x%08x\n", r1);
-    printf("r2 0x004F0382 = 0x%08x\n", r2);
-    printf("r3 0x08000002 = 0x%08x\n", r3);
-    printf("r4 0x004F0382 = 0x%08x\n", r4);
-    printf("r5 0x00300500 = 0x%08x\n", r5);
-    printf("r6 0x00300500 = 0x%08x\n", r6);
+    //----------
+    // unhalt
+
+    sl_put(DEG_Module_CTRL, 0x40000001);    
+    uint32_t r12 = sl_get(DEG_Module_STA);
+
+    //----------
+
+    printf("r1  0x%08x ?= 0x00010403\n", r1);
+    printf("r2  0x%08x ?= 0x004f0c82\n", r2);
+    printf("r3  0x%08x ?= 0x08000002\n", r3);
+    printf("r4  0x%08x ?= 0x004f0382\n", r4);
+    printf("r5  0x%08x ?= 0x00300500\n", r5);
+    printf("r6  0x%08x ?= 0x00300500\n", r6);
     printf("r7  0x%08x\n", r7);
     printf("r8  0x%08x\n", r8);
     printf("r9  0x%08x\n", r9);
     printf("r10 0x%08x\n", r10);
     printf("r11 0x%08x\n", r11);
+    printf("r12 0x%08x\n", r12);
 
     sleep_ms(1000);
   }
-
-  // if wc-link unlocks chip first
-  // ONLINE_CAPR_STA = 0x00010403
-  // DEG_Module_STA  = 0x004f0c82
-  // DEG_Abst_STA    = 0x08000302
-  // DEG_Module_STA  = 0x004f0382
-  // ???             = 0x00300500
-  // DEG_Abst_DATA0  = 0x00300500
-  // DEG_Abst_DATA0  = 0x3d78abcd
-  // DEG_Module_STA  = 0x004f0c82
-
-  // if pico talks to chip after por
-  // ONLINE_CAPR_STA = 0x00010403 ok
-  // DEG_Module_STA  = 0x004f0c82 ok
-  // DEG_Abst_STA    = 0x08000302 ok 
-  // DEG_Module_STA  = 0x004f0382 ok 
-  // ???             = 0x00300500 ok
-  // DEG_Abst_DATA0  = 0x0982021a xxx
-  // DEG_Abst_DATA0  = 0x0982021a xxx
-  // DEG_Module_STA  = 0x004f0c82 ok
-
-
-  /*
-  while(1) {
-    printf("\n");
-    printf("rep   %04d\n", rep++);
-
-    iobank0_hw->io[LED_PIN].ctrl = GPIO_FUNC_SIO << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB;
-    sio->gpio_clr    = LED_BIT;
-    sio->gpio_oe_set = LED_BIT;
-    // low pulse before rw seems to be 2 msec
-    sleep_ms(2);
-    sio->gpio_oe_clr = LED_BIT;
-
-    iobank0_hw->io[LED_PIN].ctrl = GPIO_FUNC_PIO0 << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB;
-
-    //pio_sm_set_pins_with_mask(pio0, sm, 0x00000000, 0xFFFFFFFF);
-    //sleep_ms(1);
-    //pio_sm_set_pins(pio0, sm, 1);
-    //sleep_ms(2);
-    //printf("derp\n");
-
-    pio_sm_set_enabled(pio0, sm, true);
-    slput(ONLINE_CFGR_SHAD, 0x5AA50400);
-    slput(ONLINE_CFGR_MASK, 0x5AA50400);
-    printf("ONLINE_CAPR_STA = 0x%08x\n", slget(ONLINE_CAPR_STA));
-    printf("DEG_Module_STA  = 0x%08x\n", slget(DEG_Module_STA));
-
-    // this is def "halt"
-    slput(DEG_Module_CTRL, 0x80000001);
-    slput(DEG_Module_CTRL, 0x80000001);
-    printf("DEG_Abst_STA    = 0x%08x\n", slget(DEG_Abst_STA));
-
-    printf("DEG_Module_STA  = 0x%08x\n", slget(DEG_Module_STA));
-    printf("???             = 0x%08x\n", slget(0x7F));
-
-    slput(DEG_Abst_DATA1, 0x1FFFF7C4);
-    slput(DEG_Abst_CMD, 0x00040000);
-    printf("DEG_Abst_DATA0  = 0x%08x\n", slget(DEG_Abst_DATA0));
-
-    slput(DEG_Abst_DATA1, 0x1FFFF7E8);
-    slput(DEG_Abst_CMD,   0x00040000);
-    printf("DEG_Abst_DATA0  = 0x%08x\n", slget(DEG_Abst_DATA0));
-
-		// 1101111 0 10111111 11111111 11111111 11111110 DEG_Module_CTRL := 0x40000001
-		// 1101110 1 11111111 10110000 11110011 01111101 DEG_Module_STA  == 0x004F0C82
-
-    // this is def "unhalt"
-    slput(DEG_Module_CTRL, 0x40000001);
-    printf("DEG_Module_STA  = 0x%08x\n", slget(DEG_Module_STA));
-
-    pio_sm_set_enabled(pio0, sm, false);
-    sleep_ms(1000);
-  }
-  */
 }
-
-
-
-
-
-
-
-
-
-
-#if 0
-  // WS2811 test
-  /*
-  while (1) {
-    const uint32_t colors[] = {
-      0x010000,
-      0x000100,
-      0x000001,
-      0x020000,
-      0x000200,
-      0x000002,
-      0x040000,
-      0x000400,
-      0x000004,
-      0x010101,
-    };
-
-    for (int j = 0; j < 10; j++) {
-      uint32_t blah = colors[j];
-      blah = 0xFFFFFFFF;
-      for (int i = 0; i < 24; i++) {
-        if (blah & (1 << (23 - i))) {
-          gpio_put(LED_PIN, 1);
-          DELAY_NS(700);
-          gpio_put(LED_PIN, 0);
-          DELAY_NS(600);
-        }
-        else {
-          gpio_put(LED_PIN, 1);
-          DELAY_NS(350);
-          gpio_put(LED_PIN, 0);
-          DELAY_NS(800);
-        }
-      }
-    }
-    gpio_put(LED_PIN, 0);
-    DELAY_NS(60000);
-  }
-  */
-#endif
-
-
-#if 0
-  const auto pad_hz = (3 << PADS_BANK0_GPIO0_DRIVE_LSB) | (1 << PADS_BANK0_GPIO0_SLEWFAST_LSB) | (1 << PADS_BANK0_GPIO1_IE_LSB) | (1 << PADS_BANK0_GPIO1_SCHMITT_LSB);
-  const auto pad_pu = pad_hz | (1 << PADS_BANK0_GPIO0_PUE_LSB);
-  const auto pad_pd = pad_hz | (1 << PADS_BANK0_GPIO0_PDE_LSB);
-
-  auto& pad = padsbank0_hw->io[LED_PIN];
-  pad = pad_pu;
-
-  iobank0_hw->io[LED_PIN].ctrl = GPIO_FUNC_SIO << IO_BANK0_GPIO0_CTRL_FUNCSEL_LSB;
-     sio->gpio_oe_clr = LED_BIT;
-    sio->gpio_set    = LED_BIT;
-
-  volatile uint8_t  addr = 0b10110101;
-  volatile uint32_t data = 0xA55A5A55;
-
-  while(1) {
-    //bitbang_send(addr, data);
-  }
-#endif
-
-
-#if 0
-__attribute__((noinline))
-void bitbang_send(uint8_t addr, uint32_t data) {
-  sio->gpio_oe_set = LED_BIT;
-  sio->gpio_clr    = LED_BIT;
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop;");
-
-  sio->gpio_set    = LED_BIT;
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop;");
-
-  /*
-  sio->gpio_clr    = LED_BIT;
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-
-  sio->gpio_set    = LED_BIT;
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-  asm("nop; nop; nop; nop;");
-  */
-  for (int i = 7; i >= 0; i--) {
-    if (addr & (1 << i)) {
-      sio->gpio_clr    = LED_BIT;
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop;");
-
-      sio->gpio_set    = LED_BIT;
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-    }
-    else {
-      sio->gpio_clr    = LED_BIT;
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop;");
-
-      sio->gpio_set    = LED_BIT;
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop;");
-    }
-  }
-
-  for (int i = 31; i >= 0; i--) {
-    if (data & (1 << i)) {
-      sio->gpio_clr    = LED_BIT;
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop;");
-
-      sio->gpio_set    = LED_BIT;
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-    }
-    else {
-      sio->gpio_clr    = LED_BIT;
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop;");
-
-      sio->gpio_set    = LED_BIT;
-      asm("nop; nop; nop; nop; nop; nop; nop; nop; nop; nop;");
-      asm("nop; nop; nop; nop; nop; nop; nop; nop;");
-    }
-  }
-
-
-  sio->gpio_oe_clr = LED_BIT;
-  DELAY_NS(3700);	
-}
-#endif
