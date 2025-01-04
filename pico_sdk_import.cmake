aad1af87964d550518df328622434789726731da
Plaque FCC <Plaque-FCC@github>
2025-01-04 08:31:03 +0300

    Download Pico SDK v1.5.1 instead of master branch
    If PICO_SDK_PATH is not set, the cmake script will download
     branch master of the Pico SDK, which is not compatible
     with the code in this project at the moment.
    
    Signed-off-by: Plaque FCC <Plaque-FCC@github>

--- pico_sdk_import.cmake
+++ pico_sdk_import.cmake
@@ -34,14 +34,14 @@ if (NOT PICO_SDK_PATH)
             FetchContent_Declare(
                     pico_sdk
                     GIT_REPOSITORY https://github.com/raspberrypi/pico-sdk
-                    GIT_TAG master
+                    GIT_TAG 1.5.1
                     GIT_SUBMODULES_RECURSE FALSE
             )
         else ()
             FetchContent_Declare(
                     pico_sdk
                     GIT_REPOSITORY https://github.com/raspberrypi/pico-sdk
-                    GIT_TAG master
+                    GIT_TAG 1.5.1
             )
         endif ()
 
